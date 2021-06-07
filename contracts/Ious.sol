6f28df7d215eaaf77d8696c4a87debb888050362
PunkUnknown <PunkUnknown@tutanota.com>
2021-06-07 16:53:08 +0500

    tweaks

--- contracts/Ious.sol
+++ contracts/Ious.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract IOUS {
+contract IOUS is Ownable {
     using SafeERC20 for IERC20;
 
     IERC20 debase;
@@ -13,7 +14,7 @@ contract IOUS {
 
     uint256 public debaseExchangeRate;
     uint256 public degovExchangeRate;
-    uint256 public duration;
+    bool public depositEnabled;
 
     mapping(address => uint256) public debaseDeposited;
     mapping(address => uint256) public degovDeposited;
@@ -22,31 +23,33 @@ contract IOUS {
     constructor(
         IERC20 debase_,
         IERC20 degov_,
-        uint256 duration_,
         uint256 debaseExchangeRate_,
         uint256 degovExchangeRate_
     ) {
         debase = debase_;
         degov = degov_;
 
-        duration = block.timestamp + duration_;
         debaseExchangeRate = debaseExchangeRate_;
         degovExchangeRate = degovExchangeRate_;
     }
 
-    modifier inDuration() {
-        require(block.timestamp <= duration, "IOU duration passed");
+    modifier isEnabled() {
+        require(depositEnabled);
         _;
     }
 
-    function depositDebase(uint256 amount) public inDuration {
+    function setEnable(bool depositEnabled_) public onlyOwner {
+        depositEnabled = depositEnabled_;
+    }
+
+    function depositDebase(uint256 amount) public isEnabled {
         debaseDeposited[msg.sender] += amount;
 
         uint256 iouAmount = (amount * debaseExchangeRate) / 1 ether;
         iouBalance[msg.sender] += iouAmount;
     }
 
-    function depositDegov(uint256 amount) public inDuration {
+    function depositDegov(uint256 amount) public isEnabled  {
         degovDeposited[msg.sender] += amount;
 
         uint256 iouAmount = (amount * degovExchangeRate) / 1 ether;
